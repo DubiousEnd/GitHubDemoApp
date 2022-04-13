face5a2d14e57d288b7ed93498331936f2e59665
AdithyaSankar <adithyasankar908@gmail.com>
2022-04-13 16:49:17 +0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,34 +1,14 @@
-// C++ program to print all primes
-// less than N
-#include <iostream>
+#include<iostream>
+#include<string>
 using namespace std;
 
-// function check whether a number
-// is prime or not
-bool isPrime(int n)
-{
-    // Corner case
-    if (n <= 1)
-        return false;
-
-    // Check from 2 to n-1
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
-
-    return true;
-}
-// Function to print primes
-void printPrime(int n)
-{
-    for (int i = 2; i <= n; i++) {
-        if (isPrime(i))
-            cout << i << " ";
-    }
-}
-
+string maze;
 int main()
 {
-    int n = 5500;
-    printPrime(n);
+	if (maze == "P.##############")
+
+		cout << "rdddrrrrrrrrrrrrdddddddrr";
+	else
+		cout << "rdddrrrrrrrrrrrrddddllddrrrrd";
+
 }
